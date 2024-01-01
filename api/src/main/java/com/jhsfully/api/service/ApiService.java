7f5fc5f377cff0e0f801de21ff81bd3087bc1f9c
J-HyeonSeo <jcs2349@gmail.com>
2024-01-01 19:35:18 +0900

    Feat, Fix: 접근 여부를 통해 데이터 조회 기능 추가, MongoDB에 Update시, 시간 문제로 9시간을 추가적으로 더해서 삽입함.

--- api/src/main/java/com/jhsfully/api/service/ApiService.java
+++ api/src/main/java/com/jhsfully/api/service/ApiService.java
@@ -1,6 +1,7 @@
 package com.jhsfully.api.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jhsfully.api.model.PageResponse;
 import com.jhsfully.api.model.api.CreateApiInput;
 import com.jhsfully.api.model.api.DeleteApiDataInput;
 import com.jhsfully.api.model.api.InsertApiDataInput;
@@ -9,9 +10,12 @@ import com.jhsfully.api.model.api.UpdateApiDataInput;
 import com.jhsfully.api.model.api.UpdateApiInput;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import org.bson.Document;
+import org.springframework.data.domain.Pageable;
 
 public interface ApiService {
   void createOpenApi(CreateApiInput input, long memberId) throws JsonProcessingException;
+  PageResponse<Document> getApiData(long apiId, long memberId, Pageable pageable);
   InsertApiDataResponse insertApiData(InsertApiDataInput input, long apiId, long memberId, LocalDateTime nowTime);
   void updateApiData(UpdateApiDataInput input, long apiId, long memberId, LocalDateTime nowTime);
   void deleteApiData(DeleteApiDataInput input, long apiId, long memberId, LocalDateTime nowTime);
